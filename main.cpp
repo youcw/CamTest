d2e7a6ac546569cda6f286e19a36effe3b6bf4cc
youcw <youcw0221@126.com>
2012-12-09 20:22:02 +0800

    添加摄像头视频采集，QT显示

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QtGui>
+#include "processImage.h"
+
+int main(int argc,char *argv[])
+{
+    QApplication app(argc,argv);
+    ProcessImage process;
+    process.resize(640,480);
+    process.show();
+
+    return app.exec();
+}
